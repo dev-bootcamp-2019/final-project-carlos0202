c05be0c7efd8bedb7affc68b90f18e768715140f
Carlos González <carlos0202@users.noreply.github.com>
2019-01-15 00:01:21 -0400

    Validation and photo capture added

--- client/src/components/UploadMedia.js
+++ client/src/components/UploadMedia.js
@@ -6,38 +6,59 @@ import {
     Switch
 } from "react-router-dom";
 import { connect } from "react-redux";
-import InputField, { CheckBoxField, FileField } from "./shared/Input";
+import InputField, { CheckBoxField, FileField, hasGetUserMedia, WebCamCapture } from "./shared/Input";
 import { reduxForm, Field, formValueSelector } from "redux-form";
 import * as actions from "../actions";
 
 class UploadMedia extends Component {
 
+    handleSubmit(values) {
+        console.log(values);
+    }
+
     render() {
         console.log(this.props);
+        const { pristine, reset, submitting } = this.props;
 
         return (
-            <form>
+            <form onSubmit={this.props.handleSubmit(this.handleSubmit)} noValidate>
                 <legend></legend>
                 <hr />
                 <Field name="title" id="title" type="text" inputLabel="Associated Title with the Media" component={InputField} />
                 <Field name="tags" id="tags" type="text" inputLabel="Tags that relate to the media" component={InputField} />
                 <Field name="isCameraPicture" id="isCameraPicture" type="checkbox" inputLabel="Take media from Camera?" component={CheckBoxField} />
-                {!this.props.isCameraMedia ?
-                    <Field name="selectMedia" id="selectMedia" type="file" inputLabel="Select Media File..." component={FileField} className="form-control-file" /> :
-                    <button className="btn btn-primary mb-3">Grab From Camera</button>
+                {(this.props.isCameraMedia && hasGetUserMedia()) ?
+                    <Field name="mediaFromCamera" component={WebCamCapture} /> :
+                    <Field name="selectMedia" id="selectMedia" type="file" inputLabel="Select Media File..." component={FileField} className="form-control-file" />
                 }
                 <br />
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit"  disabled={pristine || submitting} className="btn btn-outline-primary mr-sm-2">Submit</button>
+                <button type="button" disabled={pristine || submitting} onClick={reset} className="btn btn-outline-warning">Clear Values</button>
             </form>
         );
     }
 }
 
+// function to validate form values
+function validate(values) {
+    let errors = {};
 
+    errors = checkRequired(values, errors, "title", "Title");
+    errors = checkRequired(values, errors, "tags", "Tags");
+    if (values.isCameraPicture) {
+        errors = checkRequired(values, errors, "mediaFromCamera", "Media From Camera");
+    } else {
+        errors = checkRequired(values, errors, "selectMedia", "Media File to Upload");
+    }
+    console.log(errors);
 
-function validate(values){
-    const errors = {};
-    // console.log(values);
+    return errors;
+}
+
+const checkRequired = (values, errors, name, label) => {
+    if (!values[name]) {
+        errors[name] = `${label} field must have a value`;
+    }
 
     return errors;
 }
--- client/src/components/shared/Input.js
+++ client/src/components/shared/Input.js
@@ -1,12 +1,14 @@
 //contains logic to render different kinds of input fields
 import React, { Component } from 'react';
+import Webcam from "react-webcam";
 
 const InputField = ({ input, meta: { error, touched }, ...props }) => {
 
     return (
         <div className="form-group mb-3">
             <label htmlFor={props.id}>{props.inputLabel}</label>
-            <input {...input} id={props.id} name={props.id} type={props.type} className={`validate form-control ${props.className}`} />
+            <input {...input} id={props.id} name={props.id} type={props.type} 
+            className={`validate form-control ${props.className}  ${props.className} ${(touched && error != null) ? 'is-invalid' : 'is-valid'}`} />
             <div className="invalid-feedback">
                 {touched && error}
             </div>
@@ -18,7 +20,8 @@ export const CheckBoxField = ({ input, meta: { error, touched }, ...props }) => 
 
     return (
         <div className="form-group mb-3">
-            <input {...input} id={props.id} name={props.id} type={props.type} className={`validate ${props.className}`}
+            <input {...input} id={props.id} name={props.id} 
+                className={`validate ${props.className} ${(touched && error != null) ? 'is-invalid' : 'is-valid'}`}
                 type="checkbox" />
             <label htmlFor={props.id}>{props.inputLabel}</label>
             <div className="invalid-feedback">
@@ -46,7 +49,8 @@ export class FileField extends Component {
         return (
             <div className="form-group mb-3">
                 <label htmlFor={props.id}>{props.inputLabel}</label>
-                <input {...input} id={props.id} name={props.id} type="file" className={`validate form-control-file ${props.className}`}
+                <input {...input} id={props.id} name={props.id} type="file" 
+                className={`validate form-control-file ${props.className} ${(touched && error != null) ? 'is-invalid' : 'is-valid'}`}
                     onChange={this.onChange} />
                 <div className="invalid-feedback">
                     {touched && error}
@@ -54,6 +58,61 @@ export class FileField extends Component {
             </div>
         );
     }
+}
+
+export class WebCamCapture extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            screenshot: null
+        };
+    }
+    handleClick = () => {
+        const screenshot = this.webcam.getScreenshot();
+        this.setState({ screenshot });
+        this.props.input.onChange(screenshot);
+    }
+    render() {
+        const { input, meta: { error, touched }, ...props } = this.props;
+
+        return (
+            <div>
+                <div className="embed-responsive embed-responsive-4by3">
+                    <Webcam
+                        audio={false}
+                        value={this.state.screenshot}
+                        ref={node => this.webcam = node}
+                        className="embed-responsive-item"
+                        screenshotFormat="image/jpeg"
+                    />
+                    <br />
+                    <div className="text-danger mt-sm-3">
+                        {touched && error}
+                    </div>
+                </div>
+                <br />
+                <div className="form-group">
+                    <button type="button" className="btn btn-info" onClick={this.handleClick}>Capture</button>
+                </div>
+                <div className="form-group">
+                    <h4>Captured Media:</h4>
+                    <div className="embed-responsive">
+                        {this.state.screenshot ? <img className="img-fluid" src={this.state.screenshot} /> : null}
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export function hasGetUserMedia() {
+    return !!(
+        (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia
+    );
 }
 
 export default InputField;
