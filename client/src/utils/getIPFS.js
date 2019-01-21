db3bec71bfccf13dc5e7f0d95b3283ff07c4e1e1
Carlos González <carlos0202@users.noreply.github.com>
2019-01-21 17:22:28 -0400

    set heroku env var

--- client/src/utils/getIPFS.js
+++ client/src/utils/getIPFS.js
@@ -1,8 +1,10 @@
 const IPFS = require("ipfs-api");
 
-const currentConfig = process.env.REACT_APP_USE_LOCAL_IPFS ?
+const currentConfig = process.env.REACT_APP_USE_LOCAL_IPFS && !process.env.REAC_APP_HEROKU ?
     { host: 'localhost', port: '5001', readOnlyport: '8080', protocol: 'http' } :
     { host: "ipfs.infura.io",  port: '5001', readOnlyport: '8080', protocol: "https" };
+
+console.log(process.env.REACT_APP_USE_LOCAL_IPFS, process.env.REAC_APP_HEROKU);
 
 const ipfs = new IPFS(currentConfig);
 
